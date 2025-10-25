70953fad9d9e00d7d132bfc0e625c47a14f6e60d
arc360 <voidyt16@gmail.com>
2025-10-25 14:42:31 -0500

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import Home from "./Home.jsx";
 import News from "./News.jsx";
 import "./index.css"; // CSS file
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/news" element={<News />} />
-      <Route path="/adventure" element={<App />} /> {/* Use App.jsx here */}
+      <Route path="/adventure" element={<App />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
